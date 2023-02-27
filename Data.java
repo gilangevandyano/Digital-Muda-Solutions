463978576dbcf6d2b2d4fe52af5a3d561124c2b9
gilangevandyano <99318895+gilangevandyano@users.noreply.github.com>
2023-02-27 18:09:40 +0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package javaapplication1;
+
+/**
+ * @author GILANG EVANDYANO
+ *
+ */
+public record Data() {
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
